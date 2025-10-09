a2ac6dfc95f7f54940f4b9d18ba8e00af5295664
Frank-Totti <juanfrancescogarcia@gmail.com>
2025-10-09 01:03:03 -0500

    components included

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
 # Unión de todos los segmentos y/o componentes
+import time
+import tracemalloc
+
+from dinamic.dinamic import dinamic
+from brute.brute import construir_arbol
+
+def take_time_and_memory(algorithm, *args, **kwargs):
+    """Mide el tiempo y el uso de memoria en MB de una función."""
+    tracemalloc.start()
+    inicio = time.perf_counter()
+
+    result = algorithm(*args, **kwargs)
+
+    fin = time.perf_counter()
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+
+    tiempo = fin - inicio
+    memoria_usada = current / 10**6  # MB
+    memoria_pico = peak / 10**6      # MB
+
+    return {
+        "tiempo_s": tiempo,
+        "memoria_actual_MB": memoria_usada,
+        "memoria_pico_MB": memoria_pico,
+        "resultado": result
+    }
+
+
+def execute_algorithm(algorithm: str, capacities, request):
+    """Ejecuta un algoritmo específico y mide su rendimiento."""
+    if algorithm == "dinamic":
+        return take_time_and_memory(dinamic, capacities, request)
+
+    elif algorithm == "brute":
+        #return take_time_and_memory(construir_arbol, capacities, request)
+        pass
+
+    elif algorithm == "voraz":
+        # cuando implementes el algoritmo voraz, solo lo agregas aquí
+        # return take_time_and_memory(voraz, capacities, request)
+        pass
+
+    else:
+        raise ValueError(f"Algoritmo no válido: {algorithm!r}")
